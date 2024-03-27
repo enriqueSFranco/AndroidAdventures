eb79a4111090850b2c5b792a1594fec879caeee5
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-03-26 19:39:20 -0600

    main app superhero

--- app/src/main/java/com/kikesf/androidadventures/MainActivity.kt
+++ app/src/main/java/com/kikesf/androidadventures/MainActivity.kt
@@ -4,33 +4,36 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.kikesf.androidadventures.databinding.ActivityMainBinding
 import com.kikesf.androidadventures.imcApp.IMCActivity
 import com.kikesf.androidadventures.superheroApp.SuperHeroListActivity
 import com.kikesf.androidadventures.todoApp.TaskActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding // 1
 
-    private lateinit var btnIMCApp: Button
-    private lateinit var btnTodoApp: Button
-    private lateinit var btnSuperheroeApp: Button
+    private lateinit var btnAppIMC: Button
+    private lateinit var btnAppTodo: Button
+    private lateinit var btnAppSuperheroe: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // 2
+        setContentView(binding.root) // 3
         initComponents()
         initListener()
     }
 
     private fun initComponents(): Unit {
-        btnIMCApp = findViewById(R.id.app_body_mass_check)
-        btnTodoApp = findViewById(R.id.btn_todo)
-        btnSuperheroeApp = findViewById(R.id.btn_super_heroe_app)
+        btnAppIMC = findViewById(R.id.btn_app_body_mass_check)
+        btnAppTodo = findViewById(R.id.btn_app_todo)
+        btnAppSuperheroe = findViewById(R.id.btn_app_super_heroe)
     }
 
     private fun initListener(): Unit {
-        btnIMCApp.setOnClickListener { navigateToActivity(IMCActivity::class.java) }
-        btnTodoApp.setOnClickListener { navigateToActivity(TaskActivity::class.java)}
-        btnSuperheroeApp.setOnClickListener { navigateToActivity(SuperHeroListActivity::class.java) }
+        btnAppIMC.setOnClickListener { navigateToActivity(IMCActivity::class.java) }
+        btnAppTodo.setOnClickListener { navigateToActivity(TaskActivity::class.java)}
+        btnAppSuperheroe.setOnClickListener { navigateToActivity(SuperHeroListActivity::class.java) }
     }
 
     private fun navigateToActivity(activityClass: Class<*>): Unit {
