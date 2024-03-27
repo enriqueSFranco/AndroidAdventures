be698b36e51466599d64629fc55603a5ab39cdc7
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-03-26 19:39:51 -0600

    activity superhero

--- app/src/main/java/com/kikesf/androidadventures/superheroApp/SuperHeroListActivity.kt
+++ app/src/main/java/com/kikesf/androidadventures/superheroApp/SuperHeroListActivity.kt
@@ -1,17 +1,19 @@
 package com.kikesf.androidadventures.superheroApp
 
 import android.os.Bundle
-import android.widget.SearchView
+import androidx.appcompat.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.kikesf.androidadventures.R
 import com.kikesf.androidadventures.databinding.ActivitySuperHeroListBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SuperHeroListActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivitySuperHeroListBinding
+    private lateinit var retrofit: Retrofit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +21,7 @@ class SuperHeroListActivity : AppCompatActivity() {
         binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
+        retrofit = getRetrofit()
         initUI()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -30,13 +32,29 @@ class SuperHeroListActivity : AppCompatActivity() {
     }
 
     private fun initUI() {
+        binding.svSearchHeroe.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchBySuperHeroName(query.orEmpty())
+                return false
+            }
 
+            override fun onQueryTextChange(newText: String?): Boolean = false
+        })
     }
 
     private fun searchBySuperHeroName(query: String) {}
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://gateway.marvel.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
 
 // public key = 1342ee46b3a8207e80b268dc4b8f97a1 <- use
 // private key = e591b2d38c75b736f8441d4577ae5fc8706ce7e9
-
+// hash = e7538208ae63bf14cfdf1c6e4ecded44
+// base url = https://gateway.marvel.com/v1/public/
+// https://gateway.marvel.com/v1/public/comics?ts=1&apikey=1342ee46b3a8207e80b268dc4b8f97a1&hash=e7538208ae63bf14cfdf1c6e4ecded44
 // comics = GET http://gateway.marvel.com/v1/public/comics?apikey=yourAPIKEY
